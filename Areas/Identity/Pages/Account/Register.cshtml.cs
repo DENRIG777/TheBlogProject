fe076d8e7e38bd3fb90da8fca865d29a4fb3f786
DENRIG777 <62824680+DENRIG777@users.noreply.github.com>
2022-04-03 17:18:06 -0500

    working with tags part1 (httpGet/httpPost)

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using TheBlogProject.Models;
 using TheBlogProject.Services;
@@ -26,16 +29,22 @@ namespace TheBlogProject.Areas.Identity.Pages.Account
         private readonly ILogger<RegisterModel> _logger;
         //private readonly IEmailSender _emailSender;
         private readonly IBlogEmailSender _emailSender;
+        private readonly IImageService _imageService;
+        private readonly IConfiguration _configuration;
         public RegisterModel(
             UserManager<BlogUser> userManager,
             SignInManager<BlogUser> signInManager,
             ILogger<RegisterModel> logger,
-            IBlogEmailSender emailSender)
+            IBlogEmailSender emailSender,
+            IImageService imageService, 
+            IConfiguration configuration)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+            _imageService = imageService;
+            _configuration = configuration;
         }
 
         [BindProperty]
@@ -47,6 +56,9 @@ namespace TheBlogProject.Areas.Identity.Pages.Account
 
         public class InputModel
         {
+            [Display(Name = "custom Image")]
+            public IFormFile ImageFile { get; set; } 
+
             [Required]
             [StringLength(50, ErrorMessage = "The {0} must be at least {2} and at most {1} characters", MinimumLength = 2)]
             [Display(Name = "First Name")]
@@ -97,7 +109,12 @@ namespace TheBlogProject.Areas.Identity.Pages.Account
                     LastName = Input.LastName,
                     DisplayName = Input.DisplayName,
                     UserName = Input.Email, 
-                    Email = Input.Email 
+                    Email = Input.Email,
+                    ImageData = (await _imageService.EncodeImageAsync(Input.ImageFile)) ??
+                                 await _imageService.EncodeImageAsync(_configuration["DefaultUserImage"]),
+                    ContentType = Input.ImageFile is null ?
+                                Path.GetExtension(_configuration["DefaultUserImage"]) :
+                                _imageService.ContentType(Input.ImageFile)
                 };
 
                 var result = await _userManager.CreateAsync(user, Input.Password);
--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -17,11 +18,14 @@ namespace TheBlogProject.Controllers
         private readonly ApplicationDbContext _context;
         private readonly ISlugService _slugService;
         private readonly IImageService _imageService;
-        public PostsController(ApplicationDbContext context, ISlugService slugService, IImageService imageService)
+        private readonly UserManager<BlogUser> _userManager;
+
+        public PostsController(ApplicationDbContext context, ISlugService slugService, IImageService imageService, UserManager<BlogUser> userManager)
         {
             _context = context;
             _slugService = slugService;
             _imageService = imageService;
+            _userManager = userManager;
         }
 
         // GET: Posts
@@ -32,24 +36,24 @@ namespace TheBlogProject.Controllers
         }
 
         // GET: Posts/Details/5
-        //public async Task<IActionResult> Details(int? id)
-        //{
-            //if (id == null)
-            //{
-              //  return NotFound();
-           // }
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-            //var post = await _context.Posts
-              //  .Include(p => p.Blog)
-                //.Include(p => p.BlogUser)
-                //.FirstOrDefaultAsync(m => m.Id == id);
-            //if (post == null)
-           // {
-             //   return NotFound();
-           // }
+            var post = await _context.Posts
+                .Include(p => p.Blog)
+                .Include(p => p.BlogUser)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
 
-          //  return View(post);
-       // }
+            return View(post);
+        }
 
         // GET: Posts/Create
         public IActionResult Create()
@@ -70,6 +74,9 @@ namespace TheBlogProject.Controllers
             {
                 post.Created = DateTime.Now;
 
+                var authorId = _userManager.GetUserId(User);
+                post.BlogUserId = authorId;
+
                 // use the _imageService to store the incoming user specified images
                 post.ImageData = await _imageService.EncodeImageAsync(post.Image);
                 post.ContentType = _imageService.ContentType(post.Image);
@@ -87,6 +94,20 @@ namespace TheBlogProject.Controllers
 
                 _context.Add(post);
                 await _context.SaveChangesAsync();
+                //How do i loop over the incoming list of string?
+                foreach(var tagText in tagValues)
+                {
+                    _context.Add(new Tag()
+                    {
+                        PostId = post.Id,
+                        BlogUserId = authorId,
+                        Text = tagText,
+                    });
+
+                }
+
+                await _context.SaveChangesAsync();
+
                 return RedirectToAction(nameof(Index));
             }
 
